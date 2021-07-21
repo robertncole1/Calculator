66fabf49c3ac45bffcccd1873d7c4438b57a8ca4
Rob Cole <robertncole1@gmail.com>
2021-07-21 19:11:49 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose to multiply or square!");
+            var letsCalculate = Console.ReadLine();
+
+            Console.WriteLine("Enter in a string of numbers separated by commas.");
+            var stringOfNumbers = Console.ReadLine();
+
+            switch (letsCalculate.ToLower())
+            {
+                case "multiply":
+                    var multiplication = stringOfNumbers.Split(',');
+                    Console.WriteLine(int.Parse(multiplication[0]) * int.Parse(multiplication[1]) * int.Parse(multiplication[2]));
+                    break;
+                case "square":
+                    var exponents = stringOfNumbers.Split(',');
+                    Console.WriteLine(int.Parse(exponents[0]) * int.Parse(exponents[0]) + "," + int.Parse(exponents[1]) * int.Parse(exponents[1]) + "," + int.Parse(exponents[2]) * int.Parse(exponents[2]));
+                    break;
+                default:
+                    Console.WriteLine("Please enter multiply or square.");
+                    break;
+            }
+
+        }
+    }
+}
